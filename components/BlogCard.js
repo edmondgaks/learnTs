f565812471ecd49d8fdb7684c5f1521691791d9e
Edmond GAKS <edmondgaks@gmail.com>
2022-06-09 17:14:27 +0200

    adding the blogcard component

---
+++ components/BlogCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+function BlogCard({ title, author, coverPhoto, datePublished, slug }) {
+  return (
+    <div className={styles.card}></div>
+  )
+}
+
+export default BlogCard
