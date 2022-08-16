eda39a84ee8425bc697fd9dbd94eaad9550af0e5
Edmond GAKS <edmondgaks@gmail.com>
2022-08-16 13:01:18 +0200

    understanding interfaces and also with functions

--- src/index.ts
+++ src/index.ts
@@ -62,3 +62,21 @@ function addNum(x: number,y: number): number {
 function log(message: string | number): void {
     console.log(message)
 }
+
+// Interfaces
+
+interface UserInterface {
+    readonly id: number
+    name: string
+    age?: number
+}
+const user1: UserInterface = {
+    id: 1,
+    name: 'John'
+}
+
+interface MathFunc {
+    (x: number,y: number): number
+}
+const add: MathFunc = (x: number,y: number): number => x+ y
+const sub: MathFunc = (x: number,y: number): number => x - y 
