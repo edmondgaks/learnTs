23d587a23192abfb1d5c20a9d81f51f6ce0b4bdf
Edmond GAKS <edmondgaks@gmail.com>
2022-08-16 12:33:55 +0200

    understanding basic types and arrays in typescipt

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,19 @@
 "use strict";
+// Basic Types
 let id = 5;
-console.log('Id: ', id);
+let company = 'Edmond Gaks';
+let isPublished = true;
+let x = 'Hello';
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple 
+let person = ['Edmond', 1, true];
+// Tuple arrray
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Mary']
+];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+// Basic Types
 let id: number = 5
+let company: string = 'Edmond Gaks'
+let isPublished: boolean = true
+let x: any = 'Hello'
+let age: number 
+age = 30
 
-console.log('Id: ', id);
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1,true,'Hello']
+
+// Tuple 
+let person: [string, number,boolean] = ['Edmond',1,true]
+// Tuple arrray
+let employee: [number,string][]
+
+employee = [
+    [1,'Brad'],
+    [2,'John'],
+    [3,'Mary']
+]
