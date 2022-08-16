bcd3690c8d3261c435946c30e73d47207f9270db
Edmond GAKS <edmondgaks@gmail.com>
2022-08-16 14:23:29 +0200

    generic

--- dist/index.js
+++ dist/index.js
@@ -75,4 +75,9 @@ class Employee extends Person {
     }
 }
 const emp = new Employee(3, 'Shawn', 'Developer');
-console.log(emp.register());
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4, 5]);
+let strArray = getArray(['a', 'b', 'c', 'd', 'e']);
