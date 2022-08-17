19a7d059dd8037421191b1928ea4eb84ed71dd89
Edmond GAKS <edmondgaks@gmail.com>
2022-08-17 21:24:04 +0200

    using classes

--- src/index.ts
+++ src/index.ts
@@ -112,11 +112,3 @@ class Employee extends Person {
 }
 
 const emp = new Employee(3,'Shawn', 'Developer')
-
-// Generics
-function getArray<T>(items: T[]): T[] {
-    return new Array().concat(items)
-}
-let numArray = getArray<number>([1,2,3,4,5])
-let strArray = getArray<string>(['a','b','c','d','e'])
-
