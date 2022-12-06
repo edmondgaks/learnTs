fc96d0bbe0ee85ec8241f6a6cc0096fbcdf5429f
Edmond GAKS <edmondgaks@gmail.com>
2022-12-06 09:24:40 +0200

    using children props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import './App.css';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
 
 function App() {
   const personName = {
@@ -28,6 +30,8 @@ function App() {
       <Greet name="Edmond" messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='error' />
+      <Heading>Hello from the children</Heading>
     </div>
   );
 }
